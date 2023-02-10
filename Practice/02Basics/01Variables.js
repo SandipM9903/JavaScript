6c14142271bbb8b469b06c8c74a22fd4e1218497
sandipmondal990355@gmail.com <sandipmondal990355@gmail.com>
2023-02-10 13:04:31 +0530

    Solution1

---
+++ Practice/02Basics/01Variables.js
@@ -0,0 +1,12 @@
+var fullName = "Sandip Mondal"; //String
+
+var courseName = "JavaScript";  //String
+
+var isLoggedIn = true;  //Boolean Type
+
+var loggedCount = 34;   //Integer
+console.log(loggedCount);
+
+var paymentMode;
+paymentMode = "Credit Card";  
+console.log(paymentMode);
---
+++ Practice/02Basics/02Variable-two.js
@@ -0,0 +1,17 @@
+const uid = "abc3108";
+var fullName = "Sandip Mondal";
+var email = "sandip@gmail.com";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+// fullName = prompt("Enter Your Name");
+
+// console.log("Full Name is : " + fullName);
+
+console.log(`
+    With Unique ID : ${uid}
+    User is : ${fullName}
+    and his email is : ${email}
+`);
